9efaf3b51f56cdc117adf3d3283199402fdd16a9
corlen <corlenblacky@gmail.com>
2021-11-03 16:29:59 -0400

    ok

--- app.js
+++ app.js
@@ -27,6 +27,61 @@ window.onload = function () {
     });
     });
 
+    const slider = document.querySelector("#slider");
+
+    let sliderSection = document.querySelectorAll(".slider__section");
+
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+    
+    const btnleft = document.querySelector("#btn-left");
+    const btnright = document.querySelector("#btn-right");
+
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+    function next() {
+        
+        let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+        slider.style.marginLeft = "-200%";
+        slider.style.transition = "all 0.5s";
+
+        setTimeout(function () {
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+            slider.style.marginLeft = "-100%";
+            
+        }, 500);
+
+    }
+
+        function prev() {
+        
+        let sliderSection = document.querySelectorAll(".slider__section");
+        let sliderSectionLast = sliderSection[sliderSection.length - 1];
+            
+        slider.style.marginLeft = "0%";
+        slider.style.transition = "all 0.5s";
+
+        setTimeout(function () {
+            slider.style.transition = "none";
+            slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+            slider.style.marginLeft = "-100%";
+            
+        }, 500);
+
+    }
+
+    btnright.addEventListener('click', function () {
+        next();
+    });
+
+        btnleft.addEventListener('click', function () {
+        prev();
+        });
+    
+    setInterval(function() {
+        next();
+    }, 7000);
 
 };
 
