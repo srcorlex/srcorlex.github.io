2635eb677bb3be2a4188983c63048cfc6a2a2f07
corlen <corlenblacky@gmail.com>
2021-11-03 20:43:50 -0400

    money for the bill yeh

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 window.onload = function () {
-    var port = document.getElementById('portfolio');
+
+    let port = document.getElementById('portfolio');
     port.addEventListener('click', function () {
             window.scroll({
-        top: 250,
+        top: 500,
         left: 0,
         behavior: 'smooth'
     });
     });
 
-    var bio = document.getElementById('biografia');
+    let dp = document.getElementById('dp');
+    dp.addEventListener('click', function () {
+            window.scroll({
+        top: 2200,
+        left: 0,
+        behavior: 'smooth'
+    });
+    });
+
+    let bio = document.getElementById('biografia');
     bio.addEventListener('click', function () {
             window.scroll({
-        top: 2100,
+        top: 3050,
         left: 0,
         behavior: 'smooth'
     });
     });
 
     
-    var com = document.getElementById('comisiones');
+    let com = document.getElementById('comisiones');
     com.addEventListener('click', function () {
             window.scroll({
-        top: 2800,
+        top: 3700,
         left: 0,
         behavior: 'smooth'
     });
@@ -83,40 +93,8 @@ window.onload = function () {
         next();
     }, 7000);
 
-    let imagen = document.getElementsByClassName("imagen");
+    // let imagen = document.getElementsByClassName("imagen");
 
-    // imagen[0].addEventListener("click", function () {
-    //     if (imagen[0].style.transform = ("scale(1)")) {
-    //         imagen[0].style.transform = ("scale(2)");
-    //     }
-    //     else if (imagen[0].style.transform = ("scale(2)")) {
-    //         imagen[0].style.transform = ("scale(1)");
-    //     }
-    //     else {
-    //         alert("no")
-    //     };
-    // });
-
-    // imagen[0].style.transition = ("all 0.5s")
-    // imagen[0].addEventListener("click", function() {
-        // imagen[0].style.transform = ("scale(2)");
-    //     imagen[0].style.marginLeft = "30%";
-    //     // imagen[0].style.position = "absolute";
-
-    // })
-
-    // imagen[1].addEventListener("click", function() {
-    //     alert("hola");
-    // })
-
-    // imagen[2].addEventListener("click", function() {
-    //     alert("hola");
-    // })
-
-    // let body = document.getElementById("body").addEventListener("click", function() {
-    //     imagen[0,1].style.transform = ("scale(1)");
-    //     imagen[0,1].style.marginLeft = "0%";
-    // })
 
 
 };
